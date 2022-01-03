718625d05b60027e1958e2ec367454dce71f690a
Paul Tarvydas <guitarvydas@gmail.com>
2022-01-03 09:22:29 -0500

    das2f.comp

--- dev.bash
+++ dev.bash
@@ -12,18 +12,37 @@
 # wait $pid1
 
 
-# using ducts
-set -x
+# # using ducts
+# set -x
+# duct1=duct1_$RANDOM
+# duct2=duct2_$RANDOM
+# duct3=duct3_$RANDOM
+# mkfifo $duct1
+# mkfifo $duct2
+# mkfifo $duct3
+
+# ./d2f.comp 3<$duct1 4<$duct2 5>$duct3 &
+
+# str=`realpath ~/app`
+# echo $str >$duct1 &
+# echo helloworld >$duct2 &
+# cat <$duct3
+
+clear
+echo dev.bash
 duct1=duct1_$RANDOM
 duct2=duct2_$RANDOM
 duct3=duct3_$RANDOM
+duct4=duct4_$RANDOM
 mkfifo $duct1
 mkfifo $duct2
 mkfifo $duct3
+mkfifo $duct4
 
-./d2f.comp 3<$duct1 4<$duct2 5>$duct3 &
+./das2f.comp 3<$duct1 4<$duct2 5<$duct3 6>$duct4 &
+pid1=$!
 
-str=`realpath ~/app`
-echo $str >$duct1 &
+realpath ~/app >$duct1 &
 echo helloworld >$duct2 &
-cat <$duct3
+realpath ./d2f.comp >$duct3 &
+cat <$duct4
