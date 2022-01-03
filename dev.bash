bc08b3e8da510405b4512bd02367a2a47e2b7c57
Paul Tarvydas <guitarvydas@gmail.com>
2022-01-03 11:00:24 -0500

    das2py.comp

--- dev.bash
+++ dev.bash
@@ -37,13 +37,23 @@ ductD=ductD_$RANDOM
 ductE=ductE_$RANDOM
 mkfifo $ductA $ductB $ductC $ductD $ductE
 
-./das2j.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7>$ductE &
-pid1=$!
+# for das2j
+#./das2j.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7>$ductE &
+#pid1=$!
+# realpath ~/app >$ductA &
+# echo helloworld >$ductB &
+# realpath ./d2f.comp >$ductC &
+# realpath ./das2f.comp >$ductD &
+# cat <$ductE
 
+# for das2py
+./das2py.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7<$ductE &
+pid1=$!
 realpath ~/app >$ductA &
 echo helloworld >$ductB &
 realpath ./d2f.comp >$ductC &
 realpath ./das2f.comp >$ductD &
-cat <$ductE
+realpath ./das2j.comp >$ductE &
+wait $pid
 
 rm -f $ductA $ductB $ductC $ductD $ductE
