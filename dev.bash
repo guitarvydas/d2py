e172f0c36da872640a28b0d6c66652c70e0a1746
Paul Tarvydas <guitarvydas@gmail.com>
2022-01-03 08:37:13 -0500

    d2f.comp using ducts

--- dev.bash
+++ dev.bash
@@ -3,6 +3,27 @@
 # wait $pid1
 
 
-./das2f.comp `realpath ~/app` helloworld `realpath ./d2f.comp` 6>&1
-pid1=$!
-wait $pid1
+# ./das2f.comp `realpath ~/app` helloworld `realpath ./d2f.comp` 6>&1
+# pid1=$!
+# wait $pid1
+
+# ./das2j.comp `realpath ~/app` helloworld xxx `realpath ./das2f.comp` 7>&1
+# pid1=$!
+# wait $pid1
+
+
+# using ducts
+set -x
+duct1=duct1_$RANDOM
+duct2=duct2_$RANDOM
+duct3=duct3_$RANDOM
+mkfifo $duct1
+mkfifo $duct2
+mkfifo $duct3
+
+./d2f.comp 3<$duct1 4<$duct2 5>$duct3 &
+
+str=`realpath ~/app`
+echo $str >$duct1 &
+echo helloworld >$duct2 &
+cat <$duct3
