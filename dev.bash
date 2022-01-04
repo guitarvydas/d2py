959afac37c873a5a88334aa04a8ebc483d7c37c5
Paul Tarvydas <guitarvydas@gmail.com>
2022-01-04 08:34:10 -0500

    begin: reconstructing dev.bash (up to d2f)

--- dev.bash
+++ dev.bash
@@ -1,33 +1,3 @@
-# ./d2f.comp `realpath ~/app` helloworld 5>&1 &
-# pid1=$!
-# wait $pid1
-
-
-# ./das2f.comp `realpath ~/app` helloworld `realpath ./d2f.comp` 6>&1
-# pid1=$!
-# wait $pid1
-
-# ./das2j.comp `realpath ~/app` helloworld xxx `realpath ./das2f.comp` 7>&1
-# pid1=$!
-# wait $pid1
-
-
-# # using ducts
-# set -x
-# duct1=duct1_$RANDOM
-# duct2=duct2_$RANDOM
-# duct3=duct3_$RANDOM
-# mkfifo $duct1
-# mkfifo $duct2
-# mkfifo $duct3
-
-# ./d2f.comp 3<$duct1 4<$duct2 5>$duct3 &
-
-# str=`realpath ~/app`
-# echo $str >$duct1 &
-# echo helloworld >$duct2 &
-# cat <$duct3
-
 clear
 echo dev.bash
 ductA=ductA_$RANDOM
@@ -37,23 +7,40 @@ ductD=ductD_$RANDOM
 ductE=ductE_$RANDOM
 mkfifo $ductA $ductB $ductC $ductD $ductE
 
-# for das2j
-#./das2j.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7>$ductE &
-#pid1=$!
+# for d2f 
+set -x
+./d2f.comp 3<$ductA 4<$ductB 5>$ductC &
+str=`realpath ~/app`
+echo $str >$ductA &
+echo helloworld >$ductB &
+cat <$ductC
+
+# # for das2f
+# ./das2f.comp 3<$ductA 4<$ductB 5<$ductC 6>$ductD &
+# pid1=$!
 # realpath ~/app >$ductA &
 # echo helloworld >$ductB &
 # realpath ./d2f.comp >$ductC &
 # realpath ./das2f.comp >$ductD &
 # cat <$ductE
 
-# for das2py
-./das2py.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7<$ductE &
-pid1=$!
-realpath ~/app >$ductA &
-echo helloworld >$ductB &
-realpath ./d2f.comp >$ductC &
-realpath ./das2f.comp >$ductD &
-realpath ./das2j.comp >$ductE &
-wait $pid
+# # for das2j
+# ./das2j.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7>$ductE &
+# pid1=$!
+# realpath ~/app >$ductA &
+# echo helloworld >$ductB &
+# realpath ./d2f.comp >$ductC &
+# realpath ./das2f.comp >$ductD &
+# cat <$ductE
+
+# # for das2py
+# ./das2py.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7<$ductE &
+# pid1=$!
+# realpath ~/app >$ductA &
+# echo helloworld >$ductB &
+# realpath ./d2f.comp >$ductC &
+# realpath ./das2f.comp >$ductD &
+# realpath ./das2j.comp >$ductE &
+# wait $pid
 
 rm -f $ductA $ductB $ductC $ductD $ductE
