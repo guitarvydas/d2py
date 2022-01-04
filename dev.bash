5fd7f78d49f68dd6164320a3a43a653552120bb2
Paul Tarvydas <guitarvydas@gmail.com>
2022-01-04 17:30:26 -0500

    das2py emitting .py files, but no dispatcher

--- dev.bash
+++ dev.bash
@@ -23,24 +23,25 @@ mkfifo $ductA $ductB $ductC $ductD $ductE
 # realpath ./d2f.comp >$ductC &
 # cat <$ductD >das2f.fb.pl
 
-# for das2j
-./das2j.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7>$ductE &
-pid1=$!
-realpath ~/app >$ductA &
-echo helloworld >$ductB &
-realpath ./d2f.comp >$ductC &
-realpath ./das2f.comp >$ductD &
-cat <$ductE >out.json
-cat out.json
-
-# # for das2py
-# ./das2py.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7<$ductE &
+# # for das2j
+# ./das2j.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7>$ductE &
 # pid1=$!
 # realpath ~/app >$ductA &
 # echo helloworld >$ductB &
 # realpath ./d2f.comp >$ductC &
 # realpath ./das2f.comp >$ductD &
-# realpath ./das2j.comp >$ductE &
-# wait $pid
+# cat <$ductE >out.json
+# cat out.json
+
+# for das2py
+set -x
+./das2py.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7<$ductE &
+pid1=$!
+realpath ~/app >$ductA &
+echo helloworld >$ductB &
+realpath ./d2f.comp >$ductC &
+realpath ./das2f.comp >$ductD &
+realpath ./das2j.comp >$ductE &
+wait $pid
 
 rm -f $ductA $ductB $ductC $ductD $ductE
