9b611a07f175ed92a3c0c07bcded68e727440c62
Paul Tarvydas <guitarvydas@gmail.com>
2022-01-05 11:39:22 -0500

    dev.bash runs d2f only (before pre->prep)

--- dev.bash
+++ dev.bash
@@ -1,5 +1,5 @@
-echo use run.bash
-exit
+# echo use run.bash
+# exit
 
 clear
 echo dev.bash
@@ -10,13 +10,14 @@ ductD=ductD_$RANDOM
 ductE=ductE_$RANDOM
 mkfifo $ductA $ductB $ductC $ductD $ductE
 
-# # for d2f 
-# set -x
-# ./d2f.comp 3<$ductA 4<$ductB 5>$ductC &
-# str=`realpath ~/app`
-# echo $str >$ductA &
-# echo helloworld >$ductB &
-# cat <$ductC >d2f.fb.pl
+# for d2f 
+set -x
+./d2f.comp 3<$ductA 4<$ductB 5>$ductC &
+str=`realpath ~/app`
+echo $str >$ductA &
+echo helloworld >$ductB &
+cat <$ductC >d2f.fb.pl
+cat d2f.fb.pl
 
 # # for das2f
 # ./das2f.comp 3<$ductA 4<$ductB 5<$ductC 6>$ductD &
@@ -36,15 +37,15 @@ mkfifo $ductA $ductB $ductC $ductD $ductE
 # cat <$ductE >out.json
 # cat out.json
 
-# for das2py
-set -x
-./das2py.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7<$ductE &
-pid1=$!
-realpath ~/app >$ductA &
-echo helloworld >$ductB &
-realpath ./d2f.comp >$ductC &
-realpath ./das2f.comp >$ductD &
-realpath ./das2j.comp >$ductE &
-wait $pid
+# # for das2py
+# set -x
+# ./das2py.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7<$ductE &
+# pid1=$!
+# realpath ~/app >$ductA &
+# echo helloworld >$ductB &
+# realpath ./d2f.comp >$ductC &
+# realpath ./das2f.comp >$ductD &
+# realpath ./das2j.comp >$ductE &
+# wait $pid
 
 rm -f $ductA $ductB $ductC $ductD $ductE
