714c11dcadc0b8b25cd9d6f7bb8e85bdefb7bc77
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-10 18:02:05 -0500

    rearrange writing to pipes

--- generate.bash
+++ generate.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-clear
+#clear
 ductA=ductA_$RANDOM
 ductB=ductB_$RANDOM
 ductC=ductC_$RANDOM
@@ -8,13 +8,14 @@ ductE=ductE_$RANDOM
 mkfifo $ductA $ductB $ductC $ductD $ductE
 root=`realpath ../`
 
-./das2py.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7<$ductE &
-pid1=$!
 echo $root >$ductA &
 echo helloworld.drawio >$ductB &
 realpath ./d2f.comp >$ductC &
 realpath ./das2f.comp >$ductD &
 realpath ./das2j.comp >$ductE &
+
+./das2py.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7<$ductE &
+pid1=$!
 wait $pid
 
 rm -f $ductA $ductB $ductC $ductD $ductE
