0ec99283fe35c53510b81f5c5c300dce1d9ba9ca
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-11 06:50:49 -0500

    using /tmp files for ducts instead of named pipes

--- generate.bash
+++ generate.bash
@@ -1,18 +1,24 @@
 #!/bin/bash
 #clear
-ductA=ductA_$RANDOM
-ductB=ductB_$RANDOM
-ductC=ductC_$RANDOM
-ductD=ductD_$RANDOM
-ductE=ductE_$RANDOM
-mkfifo $ductA $ductB $ductC $ductD $ductE
+ductA=/tmp/ductA_$RANDOM
+ductB=/tmp/ductB_$RANDOM
+ductC=/tmp/ductC_$RANDOM
+ductD=/tmp/ductD_$RANDOM
+ductE=/tmp/ductE_$RANDOM
+#mkfifo $ductA $ductB $ductC $ductD $ductE
 root=`realpath ../`
 
-echo $root >$ductA &
-echo helloworld.drawio >$ductB &
-realpath ./d2f.comp >$ductC &
-realpath ./das2f.comp >$ductD &
-realpath ./das2j.comp | tee /tmp/ductE >$ductE &
+# echo $root >$ductA &
+# echo helloworld.drawio >$ductB &
+# realpath ./d2f.comp >$ductC &
+# realpath ./das2f.comp >$ductD &
+# realpath ./das2j.comp | tee /tmp/ductE >$ductE &
+
+echo $root >$ductA
+echo helloworld.drawio >$ductB
+realpath ./d2f.comp >$ductC
+realpath ./das2f.comp >$ductD
+realpath ./das2j.comp >$ductE
 
 ./das2py.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7<$ductE &
 pid1=$!
