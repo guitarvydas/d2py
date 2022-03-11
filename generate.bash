546361ecb09c2d35f9c77578c0eb2c4e443e8b00
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-10 22:33:00 -0500

    debug generate.bash

--- generate.bash
+++ generate.bash
@@ -12,7 +12,7 @@ echo $root >$ductA &
 echo helloworld.drawio >$ductB &
 realpath ./d2f.comp >$ductC &
 realpath ./das2f.comp >$ductD &
-realpath ./das2j.comp >$ductE &
+realpath ./das2j.comp | tee /tmp/ductE >$ductE &
 
 ./das2py.comp 3<$ductA 4<$ductB 5<$ductC 6<$ductD 7<$ductE &
 pid1=$!
